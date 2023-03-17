db25dc6a1000e57497cdad819cd01e670f8045e7
Carlos Segarra <carlos@carlossegarra.com>
2023-03-17 11:22:29 +0000

    conan: build library deps w/ conan

---
+++ product-mini/platforms/linux-cr/README.md
@@ -0,0 +1,32 @@
+# Checkpoint-Restore for WASM modules with WAMR
+
+This repository contains a proof-of-concept implementation of a library to
+provide checkpoint-restore functionality to WebAssembly modules running on
+WAMR.
+
+## Build
+
+The recommended build environment is using our development docker image:
+
+```bash
+docker run \
+  --rm -it \
+  -v $(pwd):/workspace/product-mini/platforms/linux-cr \
+  --working-dir /workspace/product-mini/platforms/linux-cr \
+  csegarragonz/wasm-micro-runtime:main \
+  bash
+```
+
+Inside the container, you may run:
+
+```bash
+# Build iwasm-cr executable
+./bin/build.sh [--clean]
+
+# Build WASM apps
+./wasm-apps/build.sh
+```
+
+## Run the demo
+
+TODO: demo not working
--- product-mini/platforms/linux-cr/bin/build.sh
+++ product-mini/platforms/linux-cr/bin/build.sh
@@ -8,11 +8,23 @@ BUILD_DIR="${PROJ_ROOT}/build"
 
 pushd ${PROJ_ROOT} >> /dev/null
 
+# Install Conan if not available
+if ! command -v conan &> /dev/null; then
+    pip3 install -U conan
+fi
+
 if [[ "$1" == "--clean" ]]; then
     rm -rf ${BUILD_DIR}
 fi
 
 mkdir -p ${BUILD_DIR}
+
+# Install project build dependencies using Conan
+conan install \
+    ${PROJ_ROOT} \
+    --profile ${PROJ_ROOT}/conan_profile.txt \
+    --output-folder=${BUILD_DIR} \
+    --build=missing
 
 pushd ${BUILD_DIR} >> /dev/null
 
@@ -22,6 +34,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=/usr/bin/clang-14 \
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++-14 \
+    -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
     ..
 cmake --build .
 
